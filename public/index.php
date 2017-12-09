54f7f769529c44cb268fa7c89fcab2cbb5673f8f
shaynereedsmith <shaynereedsmith@yahoo.com>
2017-12-09 12:00:34 -0800

    Setup files

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>thereum Account Browser SPA</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  </head>
+
+  <body>
+
+  </body>
+
+</html>
