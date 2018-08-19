a360cc49a5d02c67aa6a05fc40a07e16e867dc6a
shaynereedsmith <shaynereedsmith@yahoo.com>
2018-08-19 13:49:03 -0700

    completed functionality and began styling

--- public/index.php
+++ public/index.php
@@ -6,10 +6,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
   </head>
 
   <body>
+    <?php
+      $token = 'MEW5TTTTJKGBAG5VZRGVA12WK9PSP16PM6';
+    ?>
+    <form action="<?php echo 'https://api.etherscan.io/api?module=account&action=balance&address=0xddbd2b932c763ba5b1b7ae3b362eac3e8d40121a&tag=latest&apikey='.$token">
+      ETH Address:
+      <br>
+      <input placeholder="Enter address here..." type="text" name="eth_address"><br><br>
+      <input type="submit" value="Submit"></input>
+      <?php
+        echo '<pre>';print_r($token);die;
+      ?>
 
+    </form>
   </body>
 
 </html>
